11f4c79ce6afb652f2df9db3850db228a4f1ac2c
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-06-28 10:09:37 +0200

    Update nuget.

--- Sources/Nivaes.Compilers.Toolset/Components/Communications/ProtoBufGenerator.cs
+++ Sources/Nivaes.Compilers.Toolset/Components/Communications/ProtoBufGenerator.cs
@@ -1,81 +1,81 @@
-﻿//namespace Nivaes
-//{
-//    using System;
-//    using System.Collections.Generic;
-//    using System.Text;
-//    using Microsoft.CodeAnalysis;
-//    using Microsoft.CodeAnalysis.CSharp;
-//    using Microsoft.CodeAnalysis.CSharp.Syntax;
-//    using Microsoft.CodeAnalysis.Text;
+﻿namespace Nivaes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Text;
 
-//    [Generator]
-//    public class ProtoBufGenerator : ISourceGenerator
-//    {
-//        public void Initialize(InitializationContext context)
-//        {
-//            // Register a syntax receiver that will be created for each generation pass
-//            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+    [Generator]
+    public class ProtoBufGenerator : ISourceGenerator
+    {
+        public void Initialize(InitializationContext context)
+        {
+            // Register a syntax receiver that will be created for each generation pass
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
 
-//            Console.WriteLine("Initialize");
-//            System.Diagnostics.Debugger.Launch();
-//        }
+            Console.WriteLine("Initialize");
+            //System.Diagnostics.Debugger.Launch();
+        }
 
-//        public void Execute(SourceGeneratorContext context)
-//        {
-//            Console.WriteLine("Execute");
-//            System.Diagnostics.Debugger.Launch();
-//            // begin creating the source we'll inject into the users compilation
-//            StringBuilder sourceBuilder = new StringBuilder(@"
-//using System;
-//namespace HelloWorldGenerated
-//{
-//    public static class HelloWorld
-//    {
-//        public static void SayHello() 
-//        {
-//            Console.WriteLine(""Hello from generated code!"");
-//            Console.WriteLine(""The following syntax trees existed in the compilation that created this program:"");
-//");
+        public void Execute(SourceGeneratorContext context)
+        {
+            Console.WriteLine("Execute");
+            //System.Diagnostics.Debugger.Launch();
+            // begin creating the source we'll inject into the users compilation
+            StringBuilder sourceBuilder = new StringBuilder(@"
+using System;
+namespace Nivaes.ProtoBuf
+{
+    public static class ProtoBufHelper
+    {
+        public static void AddProtoBuf() 
+        {
+            Console.WriteLine(""Hello from generated code!"");
+            Console.WriteLine(""The following syntax trees existed in the compilation that created this program:"");
+");
 
-//            // using the context, get a list of syntax trees in the users compilation
-//            IEnumerable<SyntaxTree> syntaxTrees = context.Compilation.SyntaxTrees;
+            // using the context, get a list of syntax trees in the users compilation
+            IEnumerable<SyntaxTree> syntaxTrees = context.Compilation.SyntaxTrees;
 
-//            // add the filepath of each tree to the class we're building
-//            foreach (SyntaxTree tree in syntaxTrees)
-//            {
-//                sourceBuilder.AppendLine($@"Console.WriteLine(@"" - {tree.FilePath}"");");
-//            }
+            // add the filepath of each tree to the class we're building
+            foreach (SyntaxTree tree in syntaxTrees)
+            {
+                sourceBuilder.AppendLine($@"Console.WriteLine(@"" - {tree.FilePath}"");");
+            }
 
-//            // finish creating the source to inject
-//            sourceBuilder.Append(@"
-//        }
-//    }
-//}");
-//            System.Diagnostics.Debugger.Launch();
+            // finish creating the source to inject
+            sourceBuilder.Append(@"
+        }
+    }
+}");
+            //System.Diagnostics.Debugger.Launch();
 
-//            // inject the created source into the users compilation
-//            context.AddSource("helloWorldGenerated", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
-//        }
+            // inject the created source into the users compilation
+            context.AddSource("ProtoBufHelper", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+        }
 
-//        /// <summary>
-//        /// Created on demand before each generation pass
-//        /// </summary>
-//        private class SyntaxReceiver : ISyntaxReceiver
-//        {
-//            public List<FieldDeclarationSyntax> CandidateFields { get; } = new List<FieldDeclarationSyntax>();
+        /// <summary>
+        /// Created on demand before each generation pass
+        /// </summary>
+        private class SyntaxReceiver : ISyntaxReceiver
+        {
+            public List<FieldDeclarationSyntax> CandidateFields { get; } = new List<FieldDeclarationSyntax>();
 
-//            /// <summary>
-//            /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation
-//            /// </summary>
-//            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
-//            {
-//                // any field with at least one attribute is a candidate for property generation
-//                if (syntaxNode is FieldDeclarationSyntax fieldDeclarationSyntax
-//                    && fieldDeclarationSyntax.AttributeLists.Count > 0)
-//                {
-//                    CandidateFields.Add(fieldDeclarationSyntax);
-//                }
-//            }
-//        }
-//    }
-//}
+            /// <summary>
+            /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation
+            /// </summary>
+            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+            {
+                // any field with at least one attribute is a candidate for property generation
+                if (syntaxNode is FieldDeclarationSyntax fieldDeclarationSyntax
+                    && fieldDeclarationSyntax.AttributeLists.Count > 0)
+                {
+                    CandidateFields.Add(fieldDeclarationSyntax);
+                }
+            }
+        }
+    }
+}
