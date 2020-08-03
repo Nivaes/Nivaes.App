aa0dbde6abce4fe233ac75a275eb7980bad9e7b6
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-08-03 12:41:16 +0200

    Token

--- Sources/Nivaes.App.Compilers.Toolset/Components/Communications/ProtoBufGenerator.cs
+++ Sources/Nivaes.App.Compilers.Toolset/Components/Communications/ProtoBufGenerator.cs
@@ -2,13 +2,11 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Text;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Text;
-    using System.Diagnostics;
-    using System.IO;
 
     [Generator]
     public class ProtoBufGenerator : ISourceGenerator
