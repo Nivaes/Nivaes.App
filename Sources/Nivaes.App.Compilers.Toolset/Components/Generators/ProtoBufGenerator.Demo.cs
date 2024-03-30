c58edc9ef09c778bf3daf477a4f7d497a81d13ab
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-03-30 21:01:27 +0100

    Remove unuse packages.

--- Sources/Nivaes.App.Compilers.Toolset/Components/Generators/ProtoBufGenerator.Demo.cs
+++ Sources/Nivaes.App.Compilers.Toolset/Components/Generators/ProtoBufGenerator.Demo.cs
@@ -9,7 +9,6 @@
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Text;
-    using ProtoBuf;
 
     //[Generator]
     public class ProtoBufGeneratorDemo
