31c033f672e947bb3e44507f434f269603aff98b
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-04-27 22:03:26 +0200

    Include ConfigurationValueException class.

---
+++ Sources/Nivaes.App.Shared/Components/Exception/ConfigurationValueException.cs
@@ -0,0 +1,20 @@
+﻿namespace Nivaes.App
+{
+    public class ConfigurationValueException : Exception
+    {
+        public ConfigurationValueException()
+           : base()
+        {
+        }
+
+        public ConfigurationValueException(string? message)
+           : base($"Configuration error in: '{message}'")
+        {
+        }
+
+        public ConfigurationValueException(string? message, Exception? innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
