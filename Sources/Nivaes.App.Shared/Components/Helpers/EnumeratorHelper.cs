781f3bb99cb693a72ed88cc8dbe6566913ebe2e1
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-11-24 21:53:53 +0100

    Fixer S4456.

--- Sources/Nivaes.App.Shared/Components/Helpers/EnumeratorHelper.cs
+++ Sources/Nivaes.App.Shared/Components/Helpers/EnumeratorHelper.cs
@@ -7,8 +7,13 @@
     {
         public static IEnumerable<T> Take<T>(this IEnumerator<T> enumerator, int n)
         {
-            if (enumerator == null) throw new ArgumentNullException(nameof(enumerator));
+            ArgumentNullException.ThrowIfNull(enumerator);
 
+            return TakeIterator(enumerator, n);
+        }
+
+        private static IEnumerable<T> TakeIterator<T>(IEnumerator<T> enumerator, int n)
+        {
             int i = 0;
             while (i++ < n && enumerator.MoveNext())
             {
