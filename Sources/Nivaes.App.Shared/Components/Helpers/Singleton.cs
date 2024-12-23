f29a7107d6a06651fd6326ea3244af49c26e5470
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-12-23 18:41:20 +0100

    Sigleton and singletontest

--- Sources/Nivaes.App.Shared/Components/Helpers/Singleton.cs
+++ Sources/Nivaes.App.Shared/Components/Helpers/Singleton.cs
@@ -3,11 +3,54 @@
     using System;
     using System.Collections.Concurrent;
 
-    public static class Singleton<T>
+    public abstract class Singleton
+    {
+        protected Singleton()
+        {
+        }
+
+        protected static readonly ConcurrentDictionary<Type, Singleton> Instances = new();
+
+        public abstract void ClearInstance();
+
+        public static void Clear()
+        {
+            foreach(var instance in Instances)
+            {
+                if (Instances.TryRemove(instance.Key, out Singleton? value))
+                {
+                    value.ClearInstance();
+                }
+            }
+        }
+    }
+
+    public class Singleton<T> : Singleton
          where T : new()
     {
-        private static ConcurrentDictionary<Type, T> mInstances = new ConcurrentDictionary<Type, T>();
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S2743:Static fields should not be used in generic types", Justification = "<Pending>")]
+        private static object? mInstance;
 
-        public static T Instance => mInstances.GetOrAdd(typeof(T), (_) => new T());
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Critical Code Smell", "S2696:Instance members should not write to \"static\" fields", Justification = "<Pending>")]
+        public override void ClearInstance()
+        {
+            mInstance = null;
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (mInstance == null)
+                {
+                    Instances.GetOrAdd(typeof(T), (_) =>
+                    {
+                        mInstance = new T();
+                        return new Singleton<T>();
+                    });
+                }
+                return (T)mInstance!;
+            }
+        }
     }
 }
