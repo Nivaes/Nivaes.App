b9fba11a89ce7a7cca6fd4dfc2b9e9fb9edfd099
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-08-16 17:05:34 +0200

    Move IgnoreEventListenerAttribute

--- Sources/Nivaes.Extensions.Candidates/Components/DataModel/Attributes/IgnoreEventListenerAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Nivaes.App
-{
-    /// <summary>Attribute for indicate ignore atribute in data view.</summary>
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class IgnoreEventListenerAttribute : Attribute
-    {
-    }
-}
